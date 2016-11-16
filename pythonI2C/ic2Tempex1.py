780843a58f5294c6f7704340e0298bb5768ffd1e
youjin9209 <youjin9200@naver.com>
2016-11-16 18:23:34 +0900

    temperature alarm

--- pythonI2C/ic2Tempex1.py
+++ pythonI2C/ic2Tempex1.py
@@ -1,7 +1,8 @@
 import smbus
 import time
 import RPi.GPIO as GPIO
-
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
 bus = smbus.SMBus(1)
 addr = 0x40
 
@@ -9,24 +10,22 @@ cmd_temp = 0xf3
 cmd_humi = 0xf5
 soft_reset = 0xfe
 
-GPIO.setmode(GPIO.BCM)
-
+gpio_pin = 13
 led_pin1 = 14
 led_pin2 = 15
-
+GPIO.setup(gpio_pin, GPIO.OUT)
 GPIO.setup(led_pin1, GPIO.OUT)
 GPIO.setup(led_pin2, GPIO.OUT)
 
 temp = 0.0
 humi = 0.0
-
 val = 0
 data = [0, 0]
 
 try:
     bus.write_byte(addr, soft_reset)
     time.sleep(0.05)
-
+    p = GPIO.PWM(gpio_pin, 100)	
     while True:
         bus.write_byte(addr, cmd_temp)
         time.sleep(0.260)
@@ -44,13 +43,16 @@ try:
         humi = -6.0+125.0/65536*val;
 	
 	# if temperature is higher than 32	
-	if(temp > 32):
-		GPIO.output(led_pin1, True)
-		GPIO.output(led_pin2, True)
+	if(temp > 31):
+            p.start(100)
+            p.ChangeDutyCycle(90)
+            p.ChangeFrequency(329)
+            GPIO.output(led_pin1, True)
+	    GPIO.output(led_pin2, True)
         else:
-		GPIO.output(led_pin1, False)
-		GPIO.output(led_pin2, False)
-	
+	    GPIO.output(led_pin1, False)
+	    GPIO.output(led_pin2, False)
+	    p.stop()
         print 'temp : %.2f, humi: %.2f' %(temp, humi)
         time.sleep(1)
 except KeyboardInterrupt:
